dbea86790b20dc159b86c04e36e88b8a5184ef08
habbalmahmoud <mahhabbal.off1@gmail.com>
2023-12-20 21:46:00 +0200

    Updates

---
+++ precache-manifest.04463f7e813ea9b5d8f6437b27847781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5759931ea9551879a9b",
+    "url": "/git-todo-list/static/css/main.a49aa225.chunk.css"
+  },
+  {
+    "revision": "a5759931ea9551879a9b",
+    "url": "/git-todo-list/static/js/main.ba7bc898.chunk.js"
+  },
+  {
+    "revision": "1544399fd5911e18db0e",
+    "url": "/git-todo-list/static/js/runtime~main.46b6eaa6.js"
+  },
+  {
+    "revision": "ac6deae7d6ec5d803b76",
+    "url": "/git-todo-list/static/js/2.b64ec573.chunk.js"
+  },
+  {
+    "revision": "09ba65f46ba0e2575607",
+    "url": "/git-todo-list/static/js/3.57b62fcd.chunk.js"
+  },
+  {
+    "revision": "36f27b478ecaf52d1d10203a906ee85c",
+    "url": "/git-todo-list/index.html"
+  }
+];
